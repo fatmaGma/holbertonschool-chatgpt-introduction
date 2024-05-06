41be2d5961864a095625573e6ba3d6a1f169ec6f
fatmaGma <8891@holbertonstudents.com>
2024-05-06 09:03:48 +0000

    creation of the envrnmnt and fixing the code and test

---
+++ debugging/factorial.py
@@ -0,0 +1,34 @@
+import sys
+import math
+"""
+factorial - calculate the factorial of a given number
+@n: the number of which to calculate the factorial
+Return: integer the factorial of the input number
+"""
+
+def factorial(n):
+    if n < 0:
+        raise ValueError("Factoril is not defined for negative numbers")
+    elif n == 0:
+        return (1)
+    else:
+        result = 1
+        while n > 1:
+            result *= n
+            n -= 1
+    return result
+if len(sys.argv) != 2:
+    print("Usage: python script.py <number>")
+    sys.exit(1)
+
+try:
+    num = int (sys.argv[1])
+except ValueError:
+    print("Please provide a valid integer")
+    sys.exit(1)
+try:
+    f = factorial(num)
+    print(f)
+except ValueError as e:
+    print(e)
+    sys.exit(1)
